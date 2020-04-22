90abedec1463f3d1332ac85e931ab566271ed052
Anand Gautam <Anand.Gautam@cognizant.com>
2020-04-22 20:28:50 +0530

    adding signin/out buttons

--- src/component/GoogleAuth.js
+++ src/component/GoogleAuth.js
@@ -1,19 +1,65 @@
 import React from 'react';
 
 class GoogleAuth extends React.Component {
+    state = {
+        isSignedIn: null
+    };
+
     componentDidMount() {
         window.gapi.load('client:auth2', () => {
             window.gapi.client.init({
-                clientId: '1022581257072 - 9cdramq7gkm5kfgeerhj0ku2a7r1jhkf.apps.googleusercontent.com',
-                scope:'email'
-            })
+                clientId: '1022581257072-9cdramq7gkm5kfgeerhj0ku2a7r1jhkf.apps.googleusercontent.com',
+                scope: 'email'
+            }).then(() => {
+                this.auth = window.gapi.auth2.getAuthInstance();
+                this.onAuthChange();
+                this.auth.isSignedIn.listen(this.onAuthChange);
+            });
         });
+    }
 
+    onAuthChange = () => {
+        this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+    }
+
+    onSignin = () => {
+        this.auth.signIn();
+    };
+
+    onSignOut = () => {
+        this.auth.signOut();
+    };
+    renderSignin = () => {
+        if (this.state.isSignedIn === null) {
+            return null;
+        }
+        else if (this.state.isSignedIn) {
+            return (
+                <button
+                    className="ui red google button"
+                    onClick={this.onSignOut}
+                >
+                    <i className="google icon" />
+                    SignOut
+                </button>
+            )
+        }
+        else {
+            return (
+                <button
+                    className="ui red google button"
+                    onClick={this.onSignin}
+                >
+                    <i className="google icon" />
+                    Sign In With Google
+                </button>
+            )
+        }
     }
     render() {
         return (
             <div>
-                GoogleAuth works
+                {this.renderSignin()}
             </div>
         )
     }
