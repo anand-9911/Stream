7d1f4333119bc1631f00557b02075a63760f1c87
Anand Gautam <Anand.Gautam@cognizant.com>
2020-04-25 20:24:25 +0530

    REDUX_DEVTOOLS_EXTENSION

--- src/index.js
+++ src/index.js
@@ -4,9 +4,21 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import reducers from './reducers';
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  reducers,
+  composeEnhancer(applyMiddleware())
+);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
